54d2739b58e10dbfdc13e58dea338d7a140228b4
Renan Holler <rrholler@gmail.com>
2024-10-16 18:29:40 -0300

    Centralizado

--- src/main/java/com/simuladormalha/view/InterfaceGrafica.java
+++ src/main/java/com/simuladormalha/view/InterfaceGrafica.java
@@ -6,18 +6,27 @@ import javax.swing.*;
 import java.awt.*;
 
 public class InterfaceGrafica extends JFrame {
-    private MalhaViaria malha;
     private PainelMalha painelMalha;
 
     public InterfaceGrafica(MalhaViaria malha) {
-        this.malha = malha;
         painelMalha = new PainelMalha(malha);
 
         setTitle("Simulação de Veículos");
-        setSize(1200, 800);
+        setSize(1200, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        add(painelMalha);
+        // Definindo layout para centralizar o painel
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.CENTER;
+
+        // Adicionando o painel ao frame com as configurações do GridBagLayout
+        add(painelMalha, gbc);
+
+        // Centraliza a própria janela na tela
+        setLocationRelativeTo(null);
 
         setVisible(true);
 
@@ -25,4 +34,3 @@ public class InterfaceGrafica extends JFrame {
         new Timer(100, e -> painelMalha.repaint()).start();
     }
 }
-

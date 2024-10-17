bf93c10fbd42017eb6cdf01700eda4dfa84b9b18
Renan Holler <rrholler@gmail.com>
2024-10-17 02:40:55 -0300

    rebase

--- src/main/java/com/simulator/util/Observer/ObserverRemove.java
+++
@@ -1,8 +0,0 @@
-package com.simulator.util.Observer;
-
-import com.simulator.model.Veiculo;
-
-public interface ObserverRemove {
-
-    void remove(Veiculo veiculo);
-}
--- src/main/java/com/simulator/util/Strategy/SyncStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.simulator.util.Strategy;
-
-import com.simulator.model.Celula;
-
-public interface SyncStrategy {
-    void acquire(Celula[] celulas) throws InterruptedException;
-    void release(Celula[] celulas);
-}
